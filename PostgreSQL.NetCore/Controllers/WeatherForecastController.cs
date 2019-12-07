18feeedd850ad04999f9b22af529660d0cd8fbc9
msalah85 <eng.msalah.abdullah@gmail.com>
2019-12-07 19:06:09 +0400

    PostgresDb .net core app

---
+++ PostgreSQL.NetCore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostgreSQL.NetCore.Models;
+
+namespace PostgreSQL.NetCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+        private readonly MyWebApiContext _db;
+
+        public WeatherForecastController(//ILogger<WeatherForecastController> logger, 
+            MyWebApiContext db)
+        {
+            //_logger = logger;
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            #region Add To Postgres Db
+
+            //var group = new Group()
+            //{
+            //    Id = 1,
+            //    Name = "Admin"
+            //};
+            //_db.Groups.Add(group);
+            //_db.SaveChanges();
+
+            #endregion
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
